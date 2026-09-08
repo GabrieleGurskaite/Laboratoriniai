4d22560458e264b09d852187af4249dbb2e07f1c
GabrieleGurskaite <gurskaite.gabriele@gmail.com>
2026-09-08 13:26:07 +0300

    Enhance D1_GabrielėGurskaitė.m with examples
    Added additional MATLAB code examples and comments for clarity.

--- D1_GabrielėGurskaitė.m
+++ D1_GabrielėGurskaitė.m
@@ -27,3 +27,23 @@ docsearch title
 % LINSPACE - generuoja vienodais tarpais išsidėsčiusių skaičių seką tarp dviejų reikšmių.
 % SIZE - grąžina matricos arba masyvo matmenis (eilučių ir stulpelių skaičių).
 % MAX - suranda didžiausią masyvo reikšmę ir jos vietos indeksą.
+
+%Papildoma
+%1
+N = 0
+%2
+v = (N + 1) : 0.5 : (N + 4)
+%3
+A = [N,   N+1, N+2; 
+    N+3, N+4, N+5; 
+    N+6, N+7, N+8]
+%4
+elementas_a = A(3, 2)
+elementai_b = [A(2, 1:2); A(3, 1:2)]
+elementai_c = [A(1,1), A(1,3); A(3,1), A(3,3)]
+
+%5
+v_mod = v(1:3)'
+A_sujungta = [A, v_mod]
+
+
