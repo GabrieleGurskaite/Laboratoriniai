7aa08282002e47fc1f942c58a8ed98300cb552c6
GabrieleGurskaite <gurskaite.gabriele@gmail.com>
2026-09-08 12:59:52 +0300

    Add help and documentation commands for functions
    Added help and documentation commands for sin, plot, and title functions.

--- D1_GabrielėGurskaitė.m
+++ D1_GabrielėGurskaitė.m
@@ -8,3 +8,16 @@ plot(x, y, 'o-r', x, y/3, 'xb')
 title('Dvi funkcijos')
 xlabel('X-as')
 ylabel('F_1 [-o-]  |  F_2 [-x-]')
+
+%8 punktas
+help sin 
+doc sin 
+docsearch sin
+
+help plot
+doc plot
+docsearch plot
+
+help title
+doc title
+docsearch title
